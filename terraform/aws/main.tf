25cd42223ba35d77b0a7fbfa763af00610f729ac
pranav2329h <pranav2329h@gmail.com>
2026-09-10 14:42:05 +0530

    create the Internet Gateway + public routing layer.

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -76,3 +76,34 @@ resource "aws_subnet" "private_2" {
     Type        = "private"
   }
 }
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "blue-green-igw"
+    Project     = "blue-green-deployment-platform"
+    Environment = "dev"
+  }
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name        = "blue-green-public-rt"
+    Project     = "blue-green-deployment-platform"
+    Environment = "dev"
+  }
+}
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
